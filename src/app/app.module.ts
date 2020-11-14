eb355ce517eb0cb995d903a5b33d163e8d8b2b5f
Hector Sanchez <20186183@itla.edu.do>
2020-11-14 17:48:31 -0400

    first commit

---
+++ src/app/app.module.ts
@@ -0,0 +1,32 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+
+import { AppRoutingModule } from './app-routing.module';
+import { AppComponent } from './app.component';
+import { NavegacionComponent } from './Components/navegacion/navegacion.component';
+import { InicioComponent } from './Components/inicio/inicio.component';
+import { VuelosComponent } from './Components/vuelos/vuelos.component';
+import { NosotrosComponent } from './Components/nosotros/nosotros.component';
+import { ContactanosComponent } from './Components/contactanos/contactanos.component';
+import { LoginComponent } from './Components/login/login.component';
+import { RegistroComponent } from './Components/registro/registro.component';
+
+@NgModule({
+  declarations: [
+    AppComponent,
+    NavegacionComponent,
+    InicioComponent,
+    VuelosComponent,
+    NosotrosComponent,
+    ContactanosComponent,
+    LoginComponent,
+    RegistroComponent
+  ],
+  imports: [
+    BrowserModule,
+    AppRoutingModule
+  ],
+  providers: [],
+  bootstrap: [AppComponent]
+})
+export class AppModule { }
