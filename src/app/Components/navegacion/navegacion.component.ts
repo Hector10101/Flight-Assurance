50992260efc4cb9d26f912ad76145f9dc65ed26f
Erickps123 <erickkpsanchez@gmail.com>
2020-11-20 13:55:25 -0400

    nosotros pagina

--- src/app/Components/navegacion/navegacion.component.ts
+++ src/app/Components/navegacion/navegacion.component.ts
@@ -8,17 +8,43 @@ import {RouterModule, Routes, Router, Data } from '@angular/router';
 })
 export class NavegacionComponent implements OnInit {
 
+  public ruta: any;
   public Bundle: any = [];
   public boton: any;
   public usuarioLogueado: any;
+  
+  public inicio: string =""; vuelos: string =""; 
+  nosotros: string =""; contactanos: string ="";
 
   constructor(private router: Router) { 
+    this.ruta = router.url;
     console.log(router.url);
+    
   }
 
   ngOnInit(): void {
-   
 
+    if (this.ruta === '/'){
+      this.inicio = "active";
+    }else if (this.ruta === '/vuelos'){
+      this.vuelos = "active";
+    }else if (this.ruta === '/nosotros'){
+      this.nosotros = "active";
+    }else if (this.ruta === '/contactanos'){
+      this.contactanos = "active";
+    }
+    else if (this.ruta === '/detallesvuelo'){
+      this.vuelos = "active";
+    }
+    else if (this.ruta === '/completarDatos'){
+      this.vuelos = "active";
+    }
+    else if (this.ruta === '/pago'){
+      this.vuelos = "active";
+    }
+    else if (this.ruta === '/finalizarCompra'){
+      this.vuelos = "active";
+    }
     this.Bundle = JSON.parse(localStorage.getItem('usuario_logueado') || '{}');
     if(this.Bundle.nombre != null){
         this.boton = this.Bundle.nombre + ', ' + "Cerrar Sesion";
@@ -30,7 +56,6 @@ export class NavegacionComponent implements OnInit {
   }
 
   onclickLogueado(){
-
     if(this.usuarioLogueado){
       //limpiarstorage confirmar cerrar sesion
       localStorage.removeItem("usuario_logueado");
