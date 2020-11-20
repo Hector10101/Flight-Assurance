152ced420b0964d26d073dc3a319fbf631d469c8
Erickps123 <erickkpsanchez@gmail.com>
2020-11-20 18:47:36 -0400

    SweetAlert

--- src/app/Components/contactanos/contactanos.component.ts
+++ src/app/Components/contactanos/contactanos.component.ts
@@ -1,6 +1,7 @@
 import { Component, OnInit } from '@angular/core';
 import { RequiredValidator } from '@angular/forms';
 import { MensajeService } from 'src/app/servicios/mensaje.service';
+import swal from'sweetalert2';
 
 @Component({
   selector: 'app-contactanos',
@@ -10,12 +11,19 @@ import { MensajeService } from 'src/app/servicios/mensaje.service';
 export class ContactanosComponent implements OnInit {
 
 
+  public Nombre:any;
+  public Correo: any;
+  public Asunto: any;
+  public Mensaje: any;
   constructor(public _MessageService: MensajeService) { }
 
   ngOnInit(): void {
   } 
 
   onClickEnviar(nombre: HTMLInputElement,correo: HTMLInputElement,asunto: HTMLInputElement,mensaje: HTMLTextAreaElement){
+    
+    if(nombre.value == '' || correo.value == '' || asunto.value == '' || mensaje.value == ''){}
+    else{
     const form = {
       nombre: nombre.value,
       email: correo.value,
@@ -24,9 +32,17 @@ export class ContactanosComponent implements OnInit {
     };
 
     this._MessageService.sendMessage(form).subscribe(() => {
+      
+      swal.fire('Mensaje Enviado','Gracias por comunicarnos', 'success');
+      this.Nombre="";
+      this.Asunto="";
+      this.Correo="";
+      this.Mensaje="";
+      },error =>{
+        swal.fire('Mensaje no Enviado',':(', 'error');
       });
-   
-  }
+   }
+}
 
  
 
